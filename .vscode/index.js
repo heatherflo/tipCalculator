425b8a056ce6e115ce314735421f7bed5e612e11
Heather Floyd <heatherflo@gmail.com>
2024-03-22 10:31:53 -0600

    ignoring errors

--- .vscode/index.js
+++ .vscode/index.js
@@ -4,7 +4,9 @@ const tipInput = document.getElementById("tip");
 const total = document.getElementById("total");
 
 function calculateTotal() {
+  // @ts-ignore
   const amountValue = billInput.value;
+  // @ts-ignore
   const tipValue = tipInput.value;
   const totalValue = amountValue * (1 + tipValue / 100);
   total.innerText = totalValue.toFixed(2)
